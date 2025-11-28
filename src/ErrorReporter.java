35b0082597e09a59ffa854dcfdccbff598ccbe98
projects <rezouali.imane@gmail.com>
2025-11-28 18:51:27 +0100

    lexer error reporting

--- src/ErrorReporter.java
+++ src/ErrorReporter.java
@@ -1,3 +1,34 @@
 public class ErrorReporter {
+//Lexer error reporting utility
+    // General error reporter
+    public static String reportError(int line, int column, String message) {
+        String colStr = column > 0 ? ", column " + column : "";
+        return "Error at line " + line + colStr + ": " + message;
+    }
+
+    // Unexpected character
+    public static String reportUnexpectedChar(int line, int column, char ch) {
+        return reportError(line, column, "Unexpected character '" + ch + "'");
+    }
+
+    // Unterminated string literal
+    public static String reportUnterminatedString(int line, int column, String snippet) {
+        return reportError(line, column, "Unterminated string literal: \"" + snippet + "\"");
+    }
+
+    // Unterminated multi-line comment
+    public static String reportUnterminatedComment(int line, int column) {
+        return reportError(line, column, "Unterminated multi-line comment");
+    }
+
+    // Invalid token
+    public static String reportInvalidToken(int line, int column, String token) {
+        return reportError(line, column, "Invalid token -> " + token);
+    }
+
+    // Invalid number
+    public static String reportInvalidNumber(int line, int column, String token) {
+        return reportError(line, column, "Invalid number -> " + token);
+    }
 
 }
